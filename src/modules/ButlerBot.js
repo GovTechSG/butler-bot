8ecd90f3acd52f78aa56b09d0ee4031c5866fe17
Edison Chee <edisonchee@live.com>
2016-05-27 23:22:25 +0800

    Added inline_query response

--- src/modules/ButlerBot.js
+++ src/modules/ButlerBot.js
@@ -21,6 +21,46 @@ class ButlerBot extends TelegramBot {
       this.reply('Hello!');
     });
 
+    super.on('inline_query', function * () {
+      let results = [{
+        'type': 'article',
+        'id': 'queen_room_1',
+        'title': 'Queen Room 1',
+        'input_message_content': {
+          'message_text': '/book_queen_1',
+          'disable_web_page_preview': true
+        }
+      },
+      {
+        'type': 'article',
+        'id': 'queen_room_2',
+        'title': 'Queen Room 2',
+        'input_message_content': {
+          'message_text': '/book_queen_2',
+          'disable_web_page_preview': true
+        }
+      },
+      {
+        'type': 'article',
+        'id': 'drone_room',
+        'title': 'Drone Room',
+        'input_message_content': {
+          'message_text': '/book_drone',
+          'disable_web_page_preview': true
+        }
+      },
+      {
+        'type': 'article',
+        'id': 'fgd_room',
+        'title': 'Focus Group Discussion Room',
+        'input_message_content': {
+          'message_text': '/book_fgd',
+          'disable_web_page_preview': true
+        }
+      }]
+      this.answerInlineQuery(results);
+    })
+
     super.startPolling();
   }
 }
