a5d1f69692ef406e2b551812be25849a29dbbe66
Shek Yu Hong <yuhong90@gmail.com>
2017-07-24 22:45:37 +0800

    Removed unused USER_ID var in config

--- src/config/settings.js
+++ src/config/settings.js
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 dotenv.load();
 const CONFIG = {
 	keyFile: require.resolve(process.env.GOOGLE_KEYFILE_PATH),
-	userId: process.env.USER_ID,
 	serviceAcctId: process.env.SERVICE_ACCT_ID,
 	calendarUrl: process.env.CALENDAR_URL,
 	calendarId: JSON.parse(process.env.CALENDAR_ID),
